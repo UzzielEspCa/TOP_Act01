1c969a478ddda9b2f94424fd8f10cbd47eca7c35
UzzielEspCa <115299255+UzzielEspCa@users.noreply.github.com>
2024-01-31 10:36:34 -0600

    Add files via upload

---
+++ Saludo.java
@@ -0,0 +1,10 @@
+package com.example.topicos_act01;
+/*
+TÓPICOS AVANZADOS DE PROGRAMACIÓN - Actividad 01
+Uzziel Espinoza Calles
+* */
+public class Saludo {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo!");
+    }
+}
